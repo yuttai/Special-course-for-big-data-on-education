a779034e6191319cd29d03510d4d578a3d2bba02
yiyi0214 <zhenyi0214@gmail.com>
2023-07-24 13:07:21 +0800

    upgrade to selenium 4

--- calctest.nchu.edu.tw.selenium.py
+++ calctest.nchu.edu.tw.selenium.py
@@ -1,6 +1,7 @@
 # https://medium.com/marketingdatascience/selenium%E6%95%99%E5%AD%B8-%E4%B8%80-%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8webdriver-send-keys-988816ce9bed
 # https://medium.com/marketingdatascience/%E5%8B%95%E6%85%8B%E7%B6%B2%E9%A0%81%E7%88%AC%E8%9F%B2%E7%AC%AC%E4%BA%8C%E9%81%93%E9%8E%96-selenium%E6%95%99%E5%AD%B8-%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8find-element-s-%E5%8F%96%E5%BE%97%E7%B6%B2%E9%A0%81%E5%85%83%E7%B4%A0-%E9%99%84python-%E7%A8%8B%E5%BC%8F%E7%A2%BC-b66920fc8cab
 from selenium import webdriver, common
+from selenium.webdriver.common.by import By
 from backoff import expo, on_exception, on_predicate
 from typing import Callable
 from logging import DEBUG, basicConfig, debug
@@ -27,7 +28,7 @@ on_stale_element_reference_exception = on_exception(wait_gen=expo, exception=Sta
 
 @function_logger
 def find_buttons_by_text(web_element, text):
-    return (button for button in web_element.find_elements_by_class_name("semi-button-content") if button.text == text)
+    return (button for button in web_element.find_elements(By.CLASS_NAME,"semi-button-content") if button.text == text)
 
 
 @function_logger
@@ -61,7 +62,7 @@ driver = webdriver.Edge()
 try:
     while True:
         driver.get('http://calctest.nchu.edu.tw/')
-        driver.find_element_by_id("email").send_keys("yuttai@nchu.edu.tw")
+        driver.find_element(By.ID,"email").send_keys("yuttai@nchu.edu.tw")
         from tkinter import simpledialog, messagebox
         from ctypes import windll
         # driver.find_element_by_id("password").send_keys()
@@ -80,7 +81,7 @@ try:
     @on_predicate(wait_gen=expo, predicate=lambda x: not x)
     @function_logger
     def find_non_empty_elements_by_tag_name():
-        return driver.find_elements_by_tag_name("tr")
+        return driver.find_elements(By.TAG_NAME,"tr")
 
     @function_logger
     def grade_one_exam(exam_name):
@@ -90,7 +91,7 @@ try:
                 continue
             if not any(
                     link_text.text == exam_name
-                    for link_text in exam_tr.find_elements_by_class_name('semi-typography-link-text')):
+                    for link_text in exam_tr.find_elements(By.CLASS_NAME,'semi-typography-link-text')):
                 continue
 
             @on_stale_element_reference_exception
@@ -108,7 +109,7 @@ try:
             next(find_buttons_by_text(exam_tr, '檢視答卷')).click()
             while grade_one_student():
                 pass
-            for navigation in driver.find_elements_by_class_name("semi-navigation-item-text"):
+            for navigation in driver.find_elements(By.CLASS_NAME,"semi-navigation-item-text"):
                 if navigation.text == '考試':
                     navigation.click()
                     return
@@ -116,7 +117,7 @@ try:
     safe_click(driver, '登入')
     on_stale_element_reference_exception(lambda: next(
         div
-        for div in driver.find_elements_by_tag_name('div')
+        for div in driver.find_elements(By.TAG_NAME,'div')
         if '111-1_化工系_1225' == div.text).click())()
     while exam_name_ := simpledialog.askstring('exam name', 'Please enter the exam name we need to grade...'):
         grade_one_exam(exam_name_)
