9cb6761ed399c024001c4abab1599c2e28e18ade
Yu-Tsung Tai (戴淯琮) <yuttai@indiana.edu>
2024-05-28 05:06:08 +0800

    calctest.nchu.edu.tw.selenium.py "def grade_one_exam(exam_name)" "next(main.find_buttons_by_text(driver, "匯出結果")).click()" Added...

--- calctest.nchu.edu.tw.selenium.py
+++ calctest.nchu.edu.tw.selenium.py
@@ -1,6 +1,6 @@
 # https://medium.com/marketingdatascience/selenium%E6%95%99%E5%AD%B8-%E4%B8%80-%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8webdriver-send-keys-988816ce9bed
 # https://medium.com/marketingdatascience/%E5%8B%95%E6%85%8B%E7%B6%B2%E9%A0%81%E7%88%AC%E8%9F%B2%E7%AC%AC%E4%BA%8C%E9%81%93%E9%8E%96-selenium%E6%95%99%E5%AD%B8-%E5%A6%82%E4%BD%95%E4%BD%BF%E7%94%A8find-element-s-%E5%8F%96%E5%BE%97%E7%B6%B2%E9%A0%81%E5%85%83%E7%B4%A0-%E9%99%84python-%E7%A8%8B%E5%BC%8F%E7%A2%BC-b66920fc8cab
-from selenium import webdriver
+from selenium.webdriver import Edge, EdgeOptions
 from selenium.webdriver.common.by import By
 from backoff import expo
 from logging import DEBUG, basicConfig, debug
@@ -9,7 +9,10 @@ import main
 from time import sleep
 
 basicConfig(level=DEBUG)
-driver = webdriver.Edge()
+options = EdgeOptions()
+options.add_experimental_option("prefs", {
+    "download.default_directory": r"C:\Users\Public\Documents\StudyInIUB\Computer Science\career\國立中興大學\202402-06.微積分(二)\線上測驗系統成績"})
+driver = Edge(options=options)
 try:
     main.open_web(driver)
 
@@ -45,10 +48,11 @@ try:
                     return True
                 return False
 
-            next(main.find_buttons_by_text(exam_tr, "檢視答卷")).click()
+            main.safe_click(exam_tr, "檢視答卷")
             while grade_one_student():
                 pass
             sleep(1)
+            next(main.find_buttons_by_text(driver, "匯出結果")).click()
             for navigation in driver.find_elements(
                 By.CLASS_NAME, "semi-navigation-item-text"
             ):
